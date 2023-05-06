f2d67cf08f7481d6316437d5d2ca31da32a116cc
Vaishnavi5543 <126686430+Vaishnavi5543@users.noreply.github.com>
2023-05-06 18:51:33 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { PasswordResetComponent } from './forgot-password/forgot-password.component';
+
 
 const routes: Routes = [
   
   {path : '', redirectTo: 'login', pathMatch: 'full'},
   {path : 'login', component : LoginComponent},
-  {path : 'signup', component : SignupComponent}
+  {path : 'signup', component : SignupComponent},
+  {path : 'forgot-password', component : PasswordResetComponent}
+ 
 ];
 
 @NgModule({
